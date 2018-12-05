9f3314077fc92d50747b09931578ab5b16551bd3
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2018-12-05 19:55:10 +0530

    1e zone not available for eks

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  availability_zones_count = "${length(data.aws_availability_zones.current.names)}"
+  availability_zones_count = "${length(data.aws_availability_zones.current.names)-1}"
 
 }
